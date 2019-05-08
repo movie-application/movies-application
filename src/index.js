f7e8908fff730cbae2b6ddc78cb3c96cd0bf4c3b
rosalesnikho <rosales.nikho@gmail.com>
2019-05-08 14:11:26 -0500

    added dependencies

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getMovies} = require('./api.js')
+
+//
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
@@ -18,3 +20,39 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+// ADD Movies
+
+const addMovie = () => {
+  const blogPost = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'}; // Value from HTML
+  const url = '/posts';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(blogPost),
+  };
+  fetch(url, options)
+      .then(/* post was created successfully */)
+      .catch(/* handle errors */);
+}
+
+
+
+
+
+// Create a form for adding a new movie that has fields for the movie's title and rating
+// When the form is submitted, the page should not reload / refresh, instead, your javascript should make a POST request to /api/movies with the information the user put into the form
+
+// DELETE Movies
+
+// UPDATE Movies
+
+// RENDER Movies
+
+
+
+
+
+
