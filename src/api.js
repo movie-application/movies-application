2abb16e7d324f4db588a651101fb9e5ba6c75842
rosalesnikho <rosales.nikho@gmail.com>
2019-05-10 08:24:04 -0500

    added input styling

--- src/api.js
+++ src/api.js
@@ -1,10 +1,18 @@
 module.exports = {
+
     getMovies: () => {
         return fetch('/api/movies')
             .then(response => response.json());
     },
+
+    getMovie: (id) => {
+        return fetch(`/api/movies/${id}`, {
+            method: 'GET'
+        })
+            .then(data => data.json())
+    },
+
     addMovies: (newMovie) => {
-        console.log(newMovie);
         return fetch('/api/movies', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
@@ -12,35 +20,26 @@ module.exports = {
         })
     },
 
-    getMovie: (id) => {
-        console.log(id);
+    deleteMovie: (id) => {
         return fetch(`/api/movies/${id}`, {
-            method: 'GET'
+            method: 'DELETE'
         })
-            .then(data => data.json())
     },
 
     getPosters: (movies) => {
-
         let posterURL = 'http://www.omdbapi.com/?apikey=7a6b2b5d&t=';
         let titleSearch = movies.title;
         console.log(titleSearch);
         return fetch(posterURL + titleSearch)
     },
+
     editMovie: (id, movie) => {
-        console.log(movie.id);
         return fetch(`/api/movies/${id}`, {
             method: 'PATCH',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(movie)
         })
     },
-    deleteMovie: (id) => {
-        return fetch(`/api/movies/${id}`, {
-            method: 'DELETE'
-        })
-    }
-
 };
 
 
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const $ = require('jquery');
 
 // Global Variables
 
-let movieArray = [];
+let mArray = [];
 let renderText = '';
 let id = 0;
 
@@ -27,82 +27,79 @@ function renderMovies() {
         $('#movie_card').html('');
         movies.forEach(({title, rating, id, genre}) => {
             $('#movie_card').append
-            (`<div class="card">
-                <ul class="list-group list-group-flush movie_card">
-                    <li>ID: ${id}</li>
-                    <li><img src="http://placeholder.pics/svg/200x150" alt="placeholder image"></li>
-                    <li>Title: ${title}</li>
-                    <li>Rating: ${rating}</li>
-                    <li>Genre: ${genre}</li>
-                <li><button class="delete" value="${id}">DELETE</button></li>
-                </ul>
-            </div>`)
+            (`<div class="card animated fadeIn">
+            <ul class="list-group list-group-flush movie_card">
+                <li>ID: ${id}</li>
+                <li><h5>${title}</h5></li>
+                <li><img src="http://placeholder.pics/svg/200x150" alt="placeholder image"></li>
+                <li>Rating: ${rating}</li>
+                <li>Genre: ${genre}</li>
+                <li><button type="button" class="delete btn btn-warning" value="${id}">DELETE</button></li>
+            </ul>
+        </div>`)
         });
     })
 }
 
-function makeArray() {
-    movieArray = [];
+function makeArrayOfMovies() {
+    mArray = [];
     getMovies().then((movies) => {
         movies.forEach(function (movie) {
-            movieArray.push(movie)
+            mArray.push(movie)
         });
     });
 }
 
-function getMoviePoster (movieTitle) {
-    console.log(movieTitle.title)
-}
-
-getMoviePoster(getPosters)
-
 function searchMovies(e) {
     renderText += e.key.toLowerCase();
     let searchArray = [];
-    movieArray.forEach(function (movie) {
+    mArray.forEach(function (movie) {
         if (movie.title.toLowerCase().startsWith(renderText)) {
             searchArray.push(movie)
         } else if (movie.genre.toLocaleLowerCase().startsWith(renderText)) {
             searchArray.push(movie);
-        } else if (movie.rating == renderText) {
+        } else if (movie.rating === renderText) {
             searchArray.push(movie)
         }
     });
+
     $('#movie_card').html('');
     searchArray.forEach(({title, rating, id, genre}) => {
         $('#movie_card').append
-        (`<div class="card">
+        (`<div class="card animated fadeIn">
             <ul class="list-group list-group-flush movie_card">
-                <li>ID: ${id}</li><li>Title: ${title}</li>
+                <li>ID: ${id}</li>
+                <li><h5>${title}</h5></li>
                 <li><img src="http://placeholder.pics/svg/200x150" alt="placeholder image"></li>
                 <li>Rating: ${rating}</li>
                 <li>Genre: ${genre}</li>
-                <li><button class="delete" value="${id}">DELETE</button></li>
+                <li><button type="button" class="delete btn btn-warning" value="${id}">DELETE</button></li>
             </ul>
         </div>`)
-
     })
 }
 
 getMovies().then((movies) => {
     movies.forEach(({title, rating, id, genre}) => {
         $('#movie_card').append
-        (`<div class="card">
+        (`<div class="card animated fadeIn">
             <ul class="list-group list-group-flush movie_card">
-                <li>ID: ${id}</li><li>Title: ${title}</li>
+                <li>ID: ${id}</li>
+                <li><h5>${title}</h5></li>
                 <li><img src="http://placeholder.pics/svg/200x150" alt="placeholder image"></li>
                 <li>Rating: ${rating}</li>
                 <li>Genre: ${genre}</li>
-                <li><button class="delete" value="${id}">DELETE</button></li>
+                <li><button type="button" class="delete btn btn-warning" value="${id}">DELETE</button></li>
             </ul>
         </div>`)
     });
-    $('#loader_wrapper').addClass('hidden_loader');
-    $('#add-movie').removeClass('hidden');
-    $('#movieEdit').removeClass('hidden');
-    $('#deleteForm').removeClass('hidden');
 
-    makeArray();
+    $('#loader_wrapper').addClass('hidden_loader');
+    $('#add-movie').removeClass('hidden_class');
+    $('#movieEdit').removeClass('hidden_class');
+    $('#deleteForm').removeClass('hidden_class');
+
+    makeArrayOfMovies();
 
 }).catch((error) => {
     alert('Error')
@@ -114,10 +111,6 @@ $('#add').on('click', function (e) {
     let movieName = $('#movie-name').val();
     let rating = $('#rating').val();
     let genre = $('#genre').val();
-    $('#movie-name').val('');
-    $('#rating').val('');
-    $('#genre').val('')
-
     let newMovie = {
         title: movieName,
         rating: rating,
@@ -127,7 +120,7 @@ $('#add').on('click', function (e) {
     }).catch(function () {
     });
     renderMovies();
-    makeArray()
+    makeArrayOfMovies()
 });
 
 $('#submitId').on('click', function (e) {
@@ -136,52 +129,12 @@ $('#submitId').on('click', function (e) {
     getMovie(id).then((movie) => {
         console.log(movie);
         $('#searchResult').html
-        (`<h3>Title: ${movie.title}</h3>
-          <h3>Rating: ${movie.rating}</h3>
-          <h3>Genre: ${movie.genre}</h3>`)
-    });
-    // $('#editForm').slideDown();
-});
-
-$('#movie-submit').on('click', function (e) {
-    e.preventDefault();
-    let movieName = $('#editTitle').val();
-    let rating = $('#editRating').val();
-    let newGenre = $('#editGenre').val();
-    $('#editTitle').val('');
-    $('#editRating').val('');
-    $('#editGenre').val('');
-    let movieData = {
-        title: movieName,
-        rating: rating,
-        genre: newGenre};
-    let editId = id;
-    console.log(movieData);
-    editMovie(editId, movieData)
-        .then(console.log('Success'))
-        .catch(console.log('Error'));
-    renderMovies();
-    makeArray()
-});
-
-$('#submitDelete').on('click', function (e) {
-    e.preventDefault();
-    let movieId = $('#deleteId').val();
-    $('#deleteId').val('');
-    deleteMovie(movieId)
-        .then(console.log('Success'))
-        .catch(console.log('Error'));
-    renderMovies();
-    makeArray();
-});
-
-$('#movie_card').on('click', '.delete', function (event) {
-    let deleteID = $(event.target).val();
-    deleteMovie(deleteID).then(function () {
-        renderMovies();
-        makeArray()
+        (`<h5 class="animated bounceIn" >Title: ${movie.title}</h5>
+          <h5 class="animated bounceIn" >Rating: ${movie.rating}</h5>
+          <h5 class="animated bounceIn" >Genre: ${movie.genre}</h5>`)
     });
 });
+
 
 $('#search').on('keypress', searchMovies);
 $('#search').on('keyup', function (e) {
@@ -193,4 +146,40 @@ $('#search').on('keyup', function (e) {
             renderMovies()
         }
     }
+});
+
+$('#movie-submit').on('click', function (e) {
+    e.preventDefault();
+    let movieName = $('#editTitle').val();
+    let rating = $('#editRating').val();
+    let newGenre = $('#editGenre').val();
+    let movieData = {
+        title: movieName,
+        rating: rating,
+        genre: newGenre};
+    let editId = id;
+    console.log(movieData);
+    editMovie(editId, movieData)
+        .then(console.log('Success'))
+        .catch(console.log('Error'));
+    renderMovies();
+    makeArrayOfMovies()
+});
+
+$('#submitDelete').on('click', function (e) {
+    e.preventDefault();
+    let movieId = $('#deleteId').val();
+    deleteMovie(movieId)
+        .then(console.log('Success'))
+        .catch(console.log('Error'));
+    renderMovies();
+    makeArrayOfMovies();
+});
+
+$('#movie_card').on('click', '.delete', function (event) {
+    let deleteID = $(event.target).val();
+    deleteMovie(deleteID).then(function () {
+        renderMovies();
+        makeArrayOfMovies()
+    });
 });
